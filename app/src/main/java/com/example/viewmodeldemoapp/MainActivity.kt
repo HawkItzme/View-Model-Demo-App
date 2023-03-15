0a5c4545395583e1d22bdb4fb3a8c4c9ff9b500f
Hawk Itzme <smquasim016@gmail.com>
2023-03-16 02:19:10 +0530

    Adding Basic View Model app code

--- app/src/main/java/com/example/viewmodeldemoapp/MainActivity.kt
+++ app/src/main/java/com/example/viewmodeldemoapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.viewmodeldemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingComponent
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodeldemoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainActivityViewModel
+    lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // binding object
+        binding = DataBindingUtil.setContentView(this,
+            R.layout.activity_main)
+
+        // View Model
+        viewModel = ViewModelProvider(this)
+            .get(MainActivityViewModel::class.java)
+
+        binding.textView.text = viewModel.getCurrentCount().toString()
+
+        binding.btn.setOnClickListener(){
+            binding.textView.text = viewModel.getUpdatedCount().toString()
+        }
+
     }
 }
